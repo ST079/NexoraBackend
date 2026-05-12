aedfb8da03a60c1a9883e24680fb2ba1e6960529
Sujan Tamang <suzanyba079@gmail.com>
2026-05-12 12:08:44 +0545

    feat: Implement GraphQL error handling, user authorization, and audit logging middleware

--- src/Adapters/API/GraphQL/Users/Mutation.cs
+++ src/Adapters/API/GraphQL/Users/Mutation.cs
@@ -1,3 +1,4 @@
+using HotChocolate.Authorization;
 using NexoraBackend.Application.DTOs.Inputs.Users;
 using NexoraBackend.Application.DTOs.Responses.Auth;
 using NexoraBackend.Application.DTOs.Responses.Users;
@@ -7,15 +8,35 @@ namespace NexoraBackend.API.GraphQL.Users;
 
 public class Mutation
 {
+    [Authorize]
     public async Task<UserResponseDto> CreateUser
-    (CreateUserDto createUserDto, [Service] CreateUserUseCase createUserUseCase)
+   (CreateUserDto createUserDto, [Service] CreateUserUseCase createUserUseCase)
     {
         return await createUserUseCase.Execute(createUserDto);
     }
 
-    public async Task<LoginResponseDto> Login(LoginDto loginDto, [Service] LoginUseCase loginUseCase)
+    public async Task<LoginResponseDto> Login(LoginDto loginDto, [Service] LoginUseCase loginUseCase, [Service] IHttpContextAccessor httpContextAccessor)
     {
-        return await loginUseCase.Execute(loginDto);
+        var result = await loginUseCase.Execute(loginDto);
+        var response = httpContextAccessor.HttpContext!.Response;
+
+        response.Cookies.Append("accessToken", result.Token.AccessToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = false,
+            SameSite = SameSiteMode.Lax,
+            Expires = DateTime.UtcNow.AddMinutes(15)
+        });
+
+        response.Cookies.Append("refreshToken", result.Token.RefreshToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = false, //true for https and false for http
+            SameSite = SameSiteMode.Lax,
+            Expires = DateTime.UtcNow.AddDays(2)
+        });
+
+        return result;
     }
 
     public async Task<RegisterResponseDto> Register(RegisterDto registerDto, [Service] RegisterUserUseCase registerUseCase)
@@ -23,19 +44,28 @@ public class Mutation
         return await registerUseCase.Execute(registerDto);
     }
 
+    [Authorize]
     public async Task<UserResponseDto> UpdateUser
-    (UpdateUserDto updateUserDto, [Service] UpdateUserUseCase updateUserUseCase)
+   (UpdateUserDto updateUserDto, [Service] UpdateUserUseCase updateUserUseCase)
     {
         return await updateUserUseCase.Execute(updateUserDto);
     }
+
+    [Authorize]
     public async Task<bool> DeleteUser(Guid id, [Service] DeleteUserUseCase deleteUserUseCase)
     {
         return await deleteUserUseCase.Execute(id);
     }
 
-    public async Task<bool> Logout(string refreshToken, [Service] LogoutUseCase logoutUseCase)
+
+    [Authorize]
+    public async Task<bool> Logout(string refreshToken, [Service] LogoutUseCase logoutUseCase, [Service] IHttpContextAccessor httpContextAccessor)
     {
-        return await logoutUseCase.Execute(refreshToken);
+        await logoutUseCase.Execute(refreshToken);
+        var response = httpContextAccessor.HttpContext!.Response;
+        response.Cookies.Delete("accessToken");
+        response.Cookies.Delete("refreshToken");
+        return true;
     }
 
     public async Task<AuthResponseDto> RefreshToken(string accessToken, string refreshToken, [Service] RefreshTokenUseCase refreshTokenUseCase)
--- src/Adapters/API/Program.cs
+++ src/Adapters/API/Program.cs
@@ -1,5 +1,10 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using NexoraBackend.API.Filters;
 using NexoraBackend.API.GraphQL.Users;
+using NexoraBackend.API.Middlewares;
 using NexoraBackend.Config;
 using NexoraBackend.Core.Domain.Ports;
 using NexoraBackend.Infrastructure.Repositories;
@@ -14,6 +19,7 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<ITokenService, JwtService>();
 builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+builder.Services.AddScoped<IAuditLogRepository, AuditLogRepository>();
 
 //config
 builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("Jwt"));
@@ -22,12 +28,59 @@ builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>()
     .AddMutationType<Mutation>()
+    .AddErrorFilter<GraphQLErrorFilter>()
     .ModifyRequestOptions(opt =>
     {
         opt.IncludeExceptionDetails = true;
-    });
+    }).AddAuthorization();
 
+
+
+//Adding the http concept to add the cookie
+builder.Services.AddHttpContextAccessor();
+
+
+//Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Cookies["accessToken"];
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["Jwt:issuer"],
+        ValidAudience = builder.Configuration["Jwt:audience"],
+
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]!)
+        )
+    };
+});
+
+//build
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//middlewares
+app.UseMiddleware<AuditloggingMiddleware>();
 
 app.MapGraphQL();
 
